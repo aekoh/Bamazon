bf92b5fd23db828392872ff237e9ee1e563cd1fe
aine <aineekohh@yahoo.com>
2019-05-29 13:00:55 -0500

    CREATE TABLE

--- bamazon.sql
+++ bamazon.sql
@@ -3,4 +3,10 @@ CREATE DATABASE bamazonDB;
 
 USE bamazonDB;
 
-CREATE TABLE profucts (
+CREATE TABLE products (
+    item_id INTEGER UNIQUE,
+    product_name VARCHAR(45) NULL,
+    department_name VARCHAR (45) NULL,
+    price INTEGER,
+    stock_quantity INTEGER
+);
